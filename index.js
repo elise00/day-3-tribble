3fa2e8c6a0674e771649240d809d72df84526a81
eljc00 <elise.j.choi@gmail.com>
2018-10-03 20:42:58 -0700

    show prompt and confirm

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-console.log('hello bcpreppers!');
+
+// init; condition; POST-LOOP
+var countTo = prompt ('how high should we count even numbers');
+
+var sure = confirm('are you sure you want to count to '+ countTo);
+
+for (var i = 0; i < 101; i++){
+    if (i % 2 === 0)
+    console.log('the number is', i);
+}
+
